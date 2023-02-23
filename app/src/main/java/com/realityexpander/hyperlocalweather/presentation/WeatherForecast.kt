8edfb0c1ccb8a8b7fff88ad02c79e71355740ee1
Chris Athanas <realityexpander@gmail.com>
2023-02-23 02:25:52 -0600

    Automatically scroll forecast to current time

--- app/src/main/java/com/realityexpander/hyperlocalweather/presentation/WeatherForecast.kt
+++ app/src/main/java/com/realityexpander/hyperlocalweather/presentation/WeatherForecast.kt
@@ -3,8 +3,10 @@ package com.realityexpander.hyperlocalweather.presentation
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -28,16 +30,30 @@ fun WeatherForecast(
             )
             Spacer(modifier = Modifier.height(16.dp))
 
-            LazyRow(content = {
-                items(data) { weatherData ->
-                    HourlyWeatherDisplay(
-                        weatherData = weatherData,
-                        modifier = Modifier
-                            .height(100.dp)
-                            .padding(horizontal = 16.dp)
-                    )
+            val listState = rememberLazyListState()
+
+            LaunchedEffect(key1 = true) {
+                // get the index of the current hour
+                val index = data.indexOfFirst {
+                    it.time.hour == state.weatherInfo.currentWeatherData?.time?.hour
                 }
-            })
+                // scroll to the current hour
+                listState.animateScrollToItem(index)
+            }
+
+            LazyRow(state = listState,
+                modifier = Modifier.fillMaxWidth(),
+                content = {
+                items(data) { weatherData ->
+                        HourlyWeatherDisplay(
+                            weatherData = weatherData,
+                            modifier = Modifier
+                                .height(100.dp)
+                                .padding(horizontal = 16.dp)
+                        )
+                    }
+                }
+            )
         }
     }
 }
